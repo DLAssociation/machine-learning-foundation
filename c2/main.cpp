50fea66d78465f3648f9122fd292ae3f7dd8fc52
DLAssociation <dla.ml@outlook.com>
2015-05-28 02:52:42 +0200

    Initial Commit

---
+++ c2/main.cpp
@@ -0,0 +1,15 @@
+//
+//  main.cpp
+//  c2
+//
+//  Created by Ifly Hsueh on 28/05/15.
+//  Copyright (c) 2015 DL Association. All rights reserved.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
